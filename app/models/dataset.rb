eaed7203caba18b1f6bedc0fc582ceedae35b42c
Dimitriy Borowski <d.borowskij@gmail.com>
2026-05-23 16:41:11 +0200

    few comments for code

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -7,6 +7,9 @@ class Dataset < ApplicationRecord
   def self.search(query)
     return none if query.blank?
 
+    # Search uses ILIKE for simplicity. For large datasets I would choose
+    # PostgreSQL full-text search using tsvector and GIN index or pg_trgm gem 
+    # for typo-tolerant search
     where("title ILIKE :q OR keywords::text ILIKE :q", q: "%#{query}%")
   end
 
